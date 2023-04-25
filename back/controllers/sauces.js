d376d166ff0207507dbf7cedf23d570f34f4b853
Luca <luca.kanto@gmail.com>
2023-04-25 10:38:08 +0200

    little change

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -78,7 +78,7 @@ exports.deleteSauce = (req, res, next) => {
                 const filename = sauce.imageUrl.split('/images/')[1];
                 fs.unlink(`images/${filename}`, () => {
                     Sauce.deleteOne({ _id: req.params.id })
-                        .then(() => { res.status(200).json({ message: 'Sauce supprimés !' }) })
+                        .then(() => { res.status(200).json({ message: 'Sauce supprimé !' }) })
                         .catch(error => res.status(401).json({ error }));
                 });
             }
